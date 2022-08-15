c2815dc85751e227d27ce1f92acc078c198b1778
Zahra Mughais <zahramughais@gmail.com>
2022-08-15 09:56:40 +0300

    js class

---
+++ script.js
@@ -0,0 +1,25 @@
+class Ninja {
+    constructor (name, health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+
+    sayName(){
+        console.log(`Name: ${this.name}`)
+    }
+
+    showStats(){
+        console.log(`Name: ${this.name}, Strength:${this.strength}, Speed: ${this.speed}, Health: ${this.health}`)
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa", 0);
+ninja1.sayName();
+ninja1.drinkSake();
+ninja1.showStats();
